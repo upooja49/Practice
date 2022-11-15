6f70803b438b8df7e3bca7bff38414c15295d9b0
Pooja U <pooj3339@global.chainsys.com>
2022-11-15 10:10:24 +0530

    first commit

---
+++ src/Bus/Bus.java
@@ -0,0 +1,10 @@
+package Bus;
+
+public class Bus {
+		String busSource;
+		String busDestination;
+		String busId;
+		boolean nonStop;
+		int fare;
+
+}
---
+++ src/vehicle/Vehicle.java
@@ -0,0 +1,11 @@
+package vehicle;
+
+public class Vehicle {
+
+		String vehicleName;
+		String numberPlate;
+		String colorOfVehicle;
+		int numberOfSeats;
+		String boardingPoint;
+		
+}
